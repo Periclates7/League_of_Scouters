6f5fd5ef8bfb806d2c1e0a9ec0620db9204124c3
Pedro Jodar <pedrojodargabarron@gmail.com>
2023-03-15 16:42:20 +0100

    modificacion gitignore

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,11 +13,11 @@ st.header('SCOUTING SYSTEM FOR LEAGUE OF LEGENDS')
 
 st.subheader('Ranking de jugadores por rol')
 
-top_stats = pd.read_csv('data/cuadro_mando/top_stats_app.csv')
-jun_stats = pd.read_csv('data/cuadro_mando/jun_stats_app.csv')
-mid_stats = pd.read_csv('data/cuadro_mando/mid_stats_app.csv')
-adc_stats = pd.read_csv('data/cuadro_mando/adc_stats_app.csv')
-supp_stats = pd.read_csv('data/cuadro_mando/supp_stats_app.csv')
+top_stats = pd.read_csv('data/visualizacion/top_ss.csv')
+jun_stats = pd.read_csv('data/visualizacion/jun_ss.csv')
+mid_stats = pd.read_csv('data/visualizacion/mid_ss.csv')
+adc_stats = pd.read_csv('data/visualizacion/adc_ss.csv')
+supp_stats = pd.read_csv('data/visualizacion/supp_ss.csv')
 
 top_champ = pd.read_csv('data/cuadro_mando/top_champ_app.csv')
 jun_champ = pd.read_csv('data/cuadro_mando/jun_champ_app.csv')
@@ -29,8 +29,9 @@ dataframes_base = ['Top', 'Jungle', 'Mid', 'AD Carry', 'Support']
 dataframes_filtro = ['Stats', 'Champions']
 
 
-filtro_grafico = st.sidebar.selectbox('Selecciona una opción para visualizar', ['Selecciona un valor','Top', 'Jungle', 'Mid', 'AD Carry', 'Support'])
+filtro_grafico_player = st.sidebar.selectbox('Gráfico de jugadores', ['Selecciona un rol','Top', 'Jungle', 'Mid', 'AD Carry', 'Support'])
 
+filtro_grafico_champ = st.sidebar.selectbox('Gráfico de campeones', ['Selecciona un rol','Top', 'Jungle', 'Mid', 'AD Carry', 'Support'])
 
 
 
@@ -104,28 +105,60 @@ elif selected_dataframe == "Support":
         st.dataframe(supp_champ)
         
         
-# Si la opción seleccionada es "TOP", muestra una imagen
-if filtro_grafico == 'Top':
-    imagen = Image.open('img/grafico_top.png')
+# Selector de gráficos de mejores jugadores por rol
+
+if filtro_grafico_player == 'Top':
+    imagen = Image.open('img/p_grafico_top.png')
+    st.caption('# 10 mejores jugadores en Top')
     st.image(imagen, caption='Imagen Top', use_column_width=True)        
 
-elif filtro_grafico == 'Jungle':
-    imagen = Image.open('img/grafico_jun.png')
+elif filtro_grafico_player == 'Jungle':
+    imagen = Image.open('img/p_grafico_jun.png')
+    st.caption('# 10 mejores jugadores en Jungla')
     st.image(imagen, caption='Imagen Jun', use_column_width=True)
 
-elif filtro_grafico == 'Mid':
-    imagen = Image.open('img/grafico_mid.png')
+elif filtro_grafico_player == 'Mid':
+    imagen = Image.open('img/p_grafico_mid.png')
+    st.caption('# 10 mejores jugadores en Mid')
     st.image(imagen, caption='Imagen Mid', use_column_width=True)
 
-elif filtro_grafico == 'AD Carry':
-    imagen = Image.open('img/grafico_adc.png')
+elif filtro_grafico_player == 'AD Carry':
+    imagen = Image.open('img/p_grafico_adc.png')
+    st.caption('# 10 mejores jugadores en ADC')
     st.image(imagen, caption='Imagen ADC', use_column_width=True)
 
-elif filtro_grafico == 'Support':
-    imagen = Image.open('img/grafico_supp.png')
+elif filtro_grafico_player == 'Support':
+    imagen = Image.open('img/p_grafico_supp.png')
+    st.caption('# 10 mejores jugadores en Support')
     st.image(imagen, caption='Imagen Supp', use_column_width=True)
         
+
+# Selector de gráficos campeones más jugados por rol
         
+if filtro_grafico_champ == 'Top':
+    imagen = Image.open('img/c_grafico_top.png')
+    st.caption('# 10 campeones más jugados en Top')
+    st.image(imagen, caption='Imagen Top c', use_column_width=True)        
+
+elif filtro_grafico_champ == 'Jungle':
+    imagen = Image.open('img/c_grafico_jun.png')
+    st.caption('# 10 campeones más jugados en Jungla')
+    st.image(imagen, caption='Imagen Jun c', use_column_width=True)
+
+elif filtro_grafico_champ == 'Mid':
+    imagen = Image.open('img/c_grafico_mid.png')
+    st.caption('# 10 campeones más jugados en Mid')
+    st.image(imagen, caption='Imagen Mid c', use_column_width=True)
+
+elif filtro_grafico_champ == 'AD Carry':
+    imagen = Image.open('img/c_grafico_adc.png')
+    st.caption('# 10 campeones más jugados en ADC')
+    st.image(imagen, caption='Imagen ADC c', use_column_width=True)
+
+elif filtro_grafico_champ == 'Support':
+    imagen = Image.open('img/c_grafico_supp.png')
+    st.caption('# 10 campeones más jugados en Support')
+    st.image(imagen, caption='Imagen Supp c', use_column_width=True)
         
         
 dataframes_por_rol = {
